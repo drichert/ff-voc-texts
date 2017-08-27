8b52b71af73b99cae371ddc60849e9a6fd1a79e8
Dan Richert <dan.richert@gmail.com>
2017-08-27 17:32:01 -0400

    Adds sync version

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 var fs = require("fs")
 var glob = require("glob")
 
-module.exports = () => {
-  let texts = []
-  let globPtn = __dirname + "/text/*.txt"
+module.exports = {
+  promise: () => {
+    let texts = []
+    let globPtn = __dirname + "/text/*.txt"
 
-  let getText = (path) => {
+    let getText = (path) => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(path, { encoding: "utf8" }, (err, data) => {
+          if(err) reject(err)
+          else resolve(data.split(/(\s+)/))
+        })
+      })
+    }
+
     return new Promise((resolve, reject) => {
-      fs.readFile(path, { encoding: "utf8" }, (err, data) => {
+      glob(globPtn, (err, files) => {
         if(err) reject(err)
-        else resolve(data.split(/(\s+)/))
+        else resolve(Promise.all(files.map(getText)))
       })
     })
-  }
+  },
 
-  return new Promise((resolve, reject) => {
-    glob(globPtn, (err, files) => {
-      if(err) reject(err)
-      else resolve(Promise.all(files.map(getText)))
-    })
-  })
+  sync: () => {
+    let texts = []
+    let globPtn = __dirname + "/text/*.txt"
+
+    let getText = (path) => {
+      return fs.readFileSync(path, {
+        encoding: "utf8"
+      }).split(/(\s+)/)
+    }
+
+    let files = glob.sync(globPtn)
+
+    return files.map(getText)
+  }
 }
